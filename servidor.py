d958bbf7fa2a2a9a569983b9d9da994641e6a9c2
Samantha Silva <samanthasilva.n@gmail.com>
2017-07-01 13:44:42 -0300

    Adicionando teste para saber se está sendo enviado dados vazios do ESP.

--- servidor.py
+++ servidor.py
@@ -5,7 +5,7 @@ import sqlite3
 
 # Host como 0.0.0.0 pega o ip da máquina e fica ouvindo nele
 HOST = '0.0.0.0'
-PORT = 7374
+PORT = 7685
 
 # AF_INET = IP ; SOCK_STREAM = TCP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,34 +22,44 @@ print("Listening on %s %s" % (HOST, PORT))
 # Objeto, conexão = aceitando conexão
 (option, client) = server.accept()
 
-print("Receiving from: %s" % option)
+print("Receiving from: %s " % option)
 
 while True:
 
-    #Recebe a informação com tamanho máximo definido e
+    # Recebe a informação com tamanho máximo definido e
     # recebe apenas a mensagem enviada pelo cliente
     info = option.recv(1024).decode()
     print(info[:4])
     print(info[5:])
-    card_info = info[5:] #Pega a mensagem recebida da posição 5 até o final
-    room_info = info[:4] #Pega a mensagem recebida do começo até a posição 4
+    card_info = info[5:] #Slice/fatia - Pega a mensagem recebida da posição 5 até o final
+    room_info = info[:4] #Slice/fatia - Pega a mensagem recebida do começo até a posição 4
 
-    # Verificando se as informações existe na base
-    cursor.execute('SELECT numero_cartao, description FROM card, room WHERE numero_cartao = ? and description = ?', (card_info, room_info))
-    result = cursor.fetchall()
 
-    print(result)
+    # print(room_info + "room_info")
+    # print("card_info" + card_info )
 
-    if (result):
+    if (info != "\n"):
 
-        option.send(b'OK\n')
-        print(info)
-        print("Done!")
+        # Verificando se as informações existe na base
+        cursor.execute('SELECT numero_cartao, description FROM card, room WHERE numero_cartao = ? AND description = ?',
+                       (card_info, room_info))
+        result = cursor.fetchall()
+
+        print(result)
+
+        if (result):
+
+            option.send(b'OK\n')
+            print(info + "if")
+            print("Done!")
+
+        else:
+
+            option.send(b'REJECT\n')
+            print(info + "ELSE")
+            print("Not this time! :(")
+
 
     else:
-
-        option.send(b'REJECT\n')
-        print(info)
-        print("Not this time! :(")
-
-
+        option.send(b'Enviando dados vazios. \n Conexao encerrada.\n')
+        option.close()
