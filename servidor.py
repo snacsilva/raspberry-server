f7d689e6dc9e5a3ef9ce0db560d208b49bcf796b
Samantha Silva <samanthasilva.n@gmail.com>
2017-07-11 11:03:11 -0300

    Refatoração e ajustes no servidor.

--- servidor.py
+++ servidor.py
@@ -2,64 +2,64 @@
 import socket
 import _thread
 import sqlite3
+from datetime import time
+
 
 # Host como 0.0.0.0 pega o ip da máquina e fica ouvindo nele
+import datetime
+
 HOST = '0.0.0.0'
-PORT = 7685
+PORT = 7444 #Erro possível de ocorrer: OSError: [Errno 98] Address already in use só mudar aqui! =D
 
 # AF_INET = IP ; SOCK_STREAM = TCP
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 banco = sqlite3.connect('smart.db')
 cursor = banco.cursor()
 
 #Fica ouvindo no IP e porta definidos anteriormente
-server.bind((HOST, PORT))
+servidor.bind((HOST, PORT))
 #Escuta até 10 clientes
-server.listen(10)
+servidor.listen(10)
 print("Listening on %s %s" % (HOST, PORT))
 
 # Objeto, conexão = aceitando conexão
-(option, client) = server.accept()
+(opcao, cliente) = servidor.accept()
 
-print("Receiving from: %s " % option)
+print("Receiving from: %s " % opcao)
 
 while True:
 
     # Recebe a informação com tamanho máximo definido e
-    # recebe apenas a mensagem enviada pelo cliente
-    info = option.recv(1024).decode()
-    print(info[:4])
-    print(info[5:])
-    card_info = info[5:] #Slice/fatia - Pega a mensagem recebida da posição 5 até o final
-    room_info = info[:4] #Slice/fatia - Pega a mensagem recebida do começo até a posição 4
+    # recebe apenas a mensagem enviada pelo cliente e formata a mensagen
+    # deixando apenas a mensagem pura.
+    info = opcao.recv(1024).decode()
+
+    professor = info[5:-1] #Slice/fatia - Pega a mensagem recebida da posição 5 até o final -1
+    sala = info[:4] #Slice/fatia - Pega a mensagem recebida do começo até a posição 4
+    print(professor)
+    print(sala)
 
 
-    # print(room_info + "room_info")
-    # print("card_info" + card_info )
+    # Verificando se as informações existe na base
+    cursor.execute('SELECT matricula, numero_cartao, sala, hora_inicio FROM professor, cartao, sala, horario '
+                   'WHERE  (matricula = ? OR numero_cartao = ?) AND sala = ?', (professor, professor, sala))
+    resultado = cursor.fetchall()
 
-    if (info != "\n"):
+    print("Resultado: ")
+    print(resultado)
 
-        # Verificando se as informações existe na base
-        cursor.execute('SELECT numero_cartao, description FROM card, room WHERE numero_cartao = ? AND description = ?',
-                       (card_info, room_info))
-        result = cursor.fetchall()
+    if (resultado):
 
-        print(result)
-
-        if (result):
-
-            option.send(b'OK\n')
-            print(info + "if")
-            print("Done!")
-
-        else:
-
-            option.send(b'REJECT\n')
-            print(info + "ELSE")
-            print("Not this time! :(")
-
+        opcao.send(b'OK\n')
+        print("'--------------------------------------------------------------------'")
+        print("O professor(a) está autorizado(a) a acessar a sala.")
+        print("'--------------------------------------------------------------------'")
 
     else:
-        option.send(b'Enviando dados vazios. \n Conexao encerrada.\n')
-        option.close()
+
+        opcao.send(b'REJECT\n')
+        print("'--------------------------------------------------------------------'")
+        print("O professor(a) está autorizado(a) a acessar a sala.")
+        print("O cartão ou a matrícula: " + professor + " não está salvo no nosso BD.")
+        print("'--------------------------------------------------------------------'")
