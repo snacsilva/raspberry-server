69a431a6e807edc842e330455f3c177b354d15ad
Samantha Silva <samanthasilva.n@gmail.com>
2017-07-12 14:28:15 -0300

    Modificação de 'hora_inicio <= ?' por '? BETWEEN hora_inicio AND hora_final'.

--- servidor.py
+++ servidor.py
@@ -3,10 +3,9 @@ import socket
 import sqlite3
 from datetime import datetime, date
 
-
 # Host como 0.0.0.0 pega o ip da máquina e fica ouvindo nele
 HOST = '0.0.0.0'
-PORT = 7426 #Erro possível de ocorrer: "OSError: [Errno 98] Address already in use" só mudar aqui! =D
+PORT = 7426  # Erro possível de ocorrer: "OSError: [Errno 98] Address already in use" só mudar aqui! =D
 
 # AF_INET = IP ; SOCK_STREAM = TCP
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,16 +13,16 @@ servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 banco = sqlite3.connect('smart.db')
 cursor = banco.cursor()
 
-#Fica ouvindo no IP e porta definidos anteriormente
+# Fica ouvindo no IP e porta definidos anteriormente
 servidor.bind((HOST, PORT))
-#Escuta até 10 clientes
+# Escuta até 10 clientes
 servidor.listen(10)
-print("Ouvindo em: %s %s" % (HOST, PORT))
+print("Ouvindo em: ", (HOST, PORT))
 
 # Objeto, conexão = aceitando conexão
 (opcao, cliente) = servidor.accept()
-#Exibe o array das informações do cliente
-print("Recebendo de: %s " % opcao)
+# Exibe o array das informações do cliente
+print("Comunicando com: ", opcao)
 
 while True:
 
@@ -32,13 +31,13 @@ while True:
     # deixando apenas a mensagem pura.
     info = opcao.recv(1024).decode()
 
-    professor = info[5:-1] #Slice/fatia - Pega a mensagem recebida da posição 5 até o final -1
-    sala = info[:4] #Slice/fatia - Pega a mensagem recebida do começo até a posição 4
-    hora = datetime.now() #
-    dia = date.today() #  0=Domingo, 1=Segunda, 2=Terça, 3=Quarta, 4=Quinta, 5=Sexta, 6=Sábado.
+    professor = info[5:-1]  # Slice/fatia - Pega a mensagem recebida da posição 5 até o final -1
+    sala = info[:4]  # Slice/fatia - Pega a mensagem recebida do começo até a posição 4
+    hora = datetime.now()  #
+    dia = date.today()  # 0=Domingo, 1=Segunda, 2=Terça, 3=Quarta, 4=Quinta, 5=Sexta, 6=Sábado.
     # Pra pegar o nome da semana que tá salvo no banco já, só fazer a pesquisa pelo campo nome_dia. ^-^
 
-    #Impressões no terminal do servidor apenas para mostrar como tá saindo cada informação e como está indo para a query.
+    # Impressões no terminal do servidor apenas para mostrar como tá saindo cada informação e como está indo para a query.
     print("'--------------------------------------------------------------------'")
     print('Id do professor: ', professor)
     print('Sala: ', sala)
@@ -48,12 +47,19 @@ while True:
     print("'--------------------------------------------------------------------'")
 
     # Verificando se as informações existem na base
-    cursor.execute('SELECT matricula, numero_cartao, sala, hora_inicio, dia_semana.id as dia FROM professor, cartao, sala, horario, dia_semana '
-                   'WHERE numero_cartao = ? OR matricula = ? AND '
-                   'sala = ? AND hora_inicio <= ? AND dia = ?', (professor, professor, sala, hora.strftime("%H:%M:%S"), dia.isoweekday()))
+    cursor.execute(
+        'SELECT matricula, numero_cartao, sala, hora_inicio, id_dia AS dia '
+        'FROM professor, cartao, sala, horario, dia_semana '
+        'WHERE numero_cartao = ? OR matricula = ? '
+        'AND '
+        'sala = ? '
+        'AND '
+        '? BETWEEN hora_inicio AND hora_final '
+        'AND dia = ?',
+        (professor, professor, sala, hora.strftime("%H:%M:%S"), dia.isoweekday()))
     resultado = cursor.fetchall()
 
-    #Impressões no terminal do servidor apenas para mostrar como está vindo o array da pesquisa.
+    # Impressões no terminal do servidor apenas para mostrar como está vindo o array da pesquisa.
     print("'--------------------------------------------------------------------'")
     print("Resultado: ")
     print(resultado)
@@ -72,5 +78,5 @@ while True:
         print("'--------------------------------------------------------------------'")
         print("O professor(a) não está autorizado(a) a acessar a sala.")
         print("O cartão ou a matrícula: " + professor + " ou não está cadastrado ou não está no seu "
-                                                        "horário e dia de entrada na sala: " + sala +".")
+                                                        "horário e dia de entrada na sala: " + sala + ".")
         print("'--------------------------------------------------------------------'")
